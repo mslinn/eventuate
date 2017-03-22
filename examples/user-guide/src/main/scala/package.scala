c5403186f3b5be8b6fcfd14c1d7d007eb2da8883
Mike Slinn <mslinn@micronauticsresearch.com>
2017-03-22 15:24:15 -0700

    Made RichVectorTime, which serializes and deserializes easily

--- examples/user-guide/src/main/scala/package.scala
+++ examples/user-guide/src/main/scala/package.scala
@@ -2,6 +2,8 @@ import com.rbmhtechnology.eventuate.VectorTime
 import org.apache.commons.lang3.SerializationUtils
 
 package object sapi {
+  // todo suggest a PR that would fold this into VectorTime. Surely everyone would want this?
+  // Requires "org.apache.commons" % "commons-lang3"
   object RichVectorTime {
     def deserialize(byteArray: Array[Byte]): VectorTime =
       VectorTime(SerializationUtils.deserialize[Map[String, Long]](byteArray))
